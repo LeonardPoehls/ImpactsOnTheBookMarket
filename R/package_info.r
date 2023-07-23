58dd7d04dca80b11be2b8a1ac038c4f64c818922
LeonardPoehls <leonard.poehls@uni-ulm.de>
2023-07-23 11:53:20 +0200

    initial commit

---
+++ R/package_info.r
@@ -0,0 +1,9 @@
+ps.pkg.info = function() {
+  # Name of this package
+  package = "ImpactsOfReviewsAndCrowdRatings"
+  
+  # Name of problem sets in the package
+  ps = c("The Impacts of reviews and Ratings on Book Market")
+  
+  list(package=package, ps = ps)
+}
